4a426306d2ed5c00d906a324e47761a04fc0a772
Alexander <alexozornin97@gmail.com>
2018-01-30 17:46:02 +0500

    v 1.0.0

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const crypto = require('crypto');
+
+module.exports.get = (data, local) =>
+{
+    let salt = (Math.floor(Math.random() * (2147483647 - 256)) + 256).toString(16);
+    let primary = crypto.createHmac('sha256', local.toString(16));
+    primary.update(data);
+    let secondary = crypto.createHmac('sha224', salt);
+    secondary.update(primary.digest('hex'));
+    return { salt: salt, hash: secondary.digest('hex') };
+}
+
+module.exports.check = (data, local, salt) =>
+{
+    let primary = crypto.createHmac('sha256', local.toString(16));
+    primary.update(data);
+    let secondary = crypto.createHmac('sha224', salt);
+    secondary.update(primary.digest('hex'));
+    return { hash: secondary.digest('hex') };
+}
