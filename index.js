4ca11b6f2e4e1057b52264e5c8096587921198c8
Alexander <alexozornin97@gmail.com>
2018-03-26 14:49:51 +0500

    v 1.0.1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports.get = (data, local) =>
 {
     let salt = (Math.floor(Math.random() * (2147483647 - 256)) + 256).toString(16);
     let primary = crypto.createHmac('sha256', local.toString(16));
-    primary.update(data);
+    primary.update(data.toString());
     let secondary = crypto.createHmac('sha224', salt);
     secondary.update(primary.digest('hex'));
     return { salt: salt, hash: secondary.digest('hex') };
@@ -15,7 +15,7 @@ module.exports.get = (data, local) =>
 module.exports.check = (data, local, salt) =>
 {
     let primary = crypto.createHmac('sha256', local.toString(16));
-    primary.update(data);
+    primary.update(data.toString());
     let secondary = crypto.createHmac('sha224', salt);
     secondary.update(primary.digest('hex'));
     return { hash: secondary.digest('hex') };
